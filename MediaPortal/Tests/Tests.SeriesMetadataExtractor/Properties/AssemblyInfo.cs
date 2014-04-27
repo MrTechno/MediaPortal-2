c595b44afed8ec8a11995786d8aa5f4bf1b0bd72
MrTechno <mikew@mikew.browser.org>
2014-04-27 19:59:51 +0100

    MP2-427
    Attempt to parse series metadata from the comments

---
+++ MediaPortal/Tests/Tests.SeriesMetadataExtractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über folgende 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Tests.SeriesMetadataExtractor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("20f575ff-423c-41fc-9ea8-76ab9ebd3bdb")]
---
+++ MediaPortal/Tests/Tests.SeriesMetadataExtractor/SeriesEpisodeMatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.SeriesMetadataExtractor
+{
+  [TestClass]
+  public class SeriesEpisodeMatcher
+  {
+    [TestMethod]
+    public void SuccessTests()
+    {
+      SuccessTest();
+    }
+
+    public void SuccessTest()
+    {
+
+    }
+  }
+}
