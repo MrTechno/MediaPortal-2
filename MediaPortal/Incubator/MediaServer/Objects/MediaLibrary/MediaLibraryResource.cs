70c78bd20531ad2059d9fbda158a297f999c8ee9
MrTechno <MrTechno@users.noreply.github.com>
2017-02-12 18:50:49 +0000

    Null checks

--- MediaPortal/Incubator/MediaServer/Objects/MediaLibrary/MediaLibraryResource.cs
+++ MediaPortal/Incubator/MediaServer/Objects/MediaLibrary/MediaLibraryResource.cs
@@ -65,26 +65,26 @@ namespace MediaPortal.Plugins.MediaServer.Objects.MediaLibrary
       var dlnaProtocolInfo = DlnaProtocolInfoFactory.GetProfileInfo(dlnaItem, Client.Profile.ProtocolInfo);
       if (dlnaProtocolInfo != null)
         ProtocolInfo = dlnaProtocolInfo.ToString();
-      if (dlnaItem.DlnaMetadata.Metadata.Size > 0)
+      if (dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null && dlnaItem.DlnaMetadata.Metadata.Size > 0)
       {
         Size = Convert.ToUInt64(dlnaItem.DlnaMetadata.Metadata.Size);
       }
       if (dlnaItem.IsImage == false)
       {
-        if (dlnaItem.DlnaMetadata.Metadata.Bitrate > 0)
+        if (dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null && dlnaItem.DlnaMetadata.Metadata.Bitrate > 0)
           BitRate = Convert.ToUInt32((double)dlnaItem.DlnaMetadata.Metadata.Bitrate / 8.0);
-        if (dlnaItem.DlnaMetadata.Audio[0].Frequency > 0)
+        if (dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null && dlnaItem.DlnaMetadata.Audio[0].Frequency > 0)
           SampleFrequency = Convert.ToUInt32(dlnaItem.DlnaMetadata.Audio[0].Frequency);
-        if (dlnaItem.DlnaMetadata.Audio[0].Channels > 0)
+        if (dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null && dlnaItem.DlnaMetadata.Audio[0].Channels > 0)
           NumberOfAudioChannels = Convert.ToUInt32(dlnaItem.DlnaMetadata.Audio[0].Channels);
-        if(dlnaItem.DlnaMetadata.Metadata.Duration > 0)
+        if(dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null && dlnaItem.DlnaMetadata.Metadata.Duration > 0)
           Duration = TimeSpan.FromSeconds(dlnaItem.DlnaMetadata.Metadata.Duration).ToString(@"hh\:mm\:ss\.fff");
       }
-      if(dlnaItem.IsVideo == true)
+      if(dlnaItem.IsVideo && dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null)
       {
         Resolution = dlnaItem.DlnaMetadata.Video.Width + "x" + dlnaItem.DlnaMetadata.Video.Height;
       }
-      else if (dlnaItem.IsImage == true)
+      else if (dlnaItem.IsImage && dlnaItem.DlnaMetadata != null && dlnaItem.DlnaMetadata.Metadata != null)
       {
         Resolution = dlnaItem.DlnaMetadata.Image.Width + "x" + dlnaItem.DlnaMetadata.Image.Height;
       }
