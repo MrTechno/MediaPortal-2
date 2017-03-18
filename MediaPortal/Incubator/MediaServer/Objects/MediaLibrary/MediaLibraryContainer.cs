68dd536a0d3aa0d37ade0583375d6d32c10ef3b9
MrTechno <MrTechno@users.noreply.github.com>
2017-03-18 23:29:28 +0000

    Added DLNA files back

---
+++ MediaPortal/Incubator/MediaServer/Objects/MediaLibrary/MediaLibraryContainer.cs
@@ -0,0 +1,100 @@
+﻿#region Copyright (C) 2007-2012 Team MediaPortal
+
+/*
+    Copyright (C) 2007-2012 Team MediaPortal
+    http://www.team-mediaportal.com
+
+    This file is part of MediaPortal 2
+
+    MediaPortal 2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MediaPortal 2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MediaPortal.Backend.MediaLibrary;
+using MediaPortal.Common;
+using MediaPortal.Common.Logging;
+using MediaPortal.Common.MediaManagement;
+using MediaPortal.Common.MediaManagement.DefaultItemAspects;
+using MediaPortal.Plugins.MediaServer.Objects.Basic;
+using MediaPortal.Plugins.MediaServer.Profiles;
+using MediaPortal.Plugins.Transcoding.Interfaces.Aspects;
+
+namespace MediaPortal.Plugins.MediaServer.Objects.MediaLibrary
+{
+  public class MediaLibraryContainer : BasicContainer
+  {
+    public MediaItem Item { get; protected set; }
+
+    public MediaLibraryContainer(string baseKey, MediaItem item, EndPointSettings client)
+      : base(baseKey + ":" + item.MediaItemId, client)
+    {
+      Item = item;
+    }
+
+    public override int ChildCount { get; set; }
+
+    public override void Initialise()
+    {
+      string value;
+      if (MediaItemAspect.TryGetAttribute(Item.Aspects, MediaAspect.ATTR_TITLE, out value))
+      {
+        Title = value;
+      }
+      ChildCount = MediaLibraryBrowse().Count;
+    }
+
+    private ICollection<MediaItem> MediaLibraryBrowse()
+    {
+      var necessaryMIATypeIDs = new Guid[]
+                                  {
+                                    ProviderResourceAspect.ASPECT_ID,
+                                    MediaAspect.ASPECT_ID,
+                                  };
+      var optionalMIATypeIDs = new Guid[]
+                                 {
+                                   DirectoryAspect.ASPECT_ID,
+                                   VideoAspect.ASPECT_ID,
+                                   AudioAspect.ASPECT_ID,
+                                   ImageAspect.ASPECT_ID,
+                                   TranscodeItemAudioAspect.ASPECT_ID,
+                                   TranscodeItemImageAspect.ASPECT_ID,
+                                   TranscodeItemVideoAspect.ASPECT_ID,
+                                 };
+
+      var library = ServiceRegistration.Get<IMediaLibrary>();
+      return library.Browse(Item.MediaItemId, necessaryMIATypeIDs, optionalMIATypeIDs, null, true);
+    }
+
+    public override List<IDirectoryObject> Search(string filter, string sortCriteria)
+    {
+      var result = new List<IDirectoryObject>();
+      foreach (var item in MediaLibraryBrowse())
+      {
+        try
+        {
+          result.Add(MediaLibraryHelper.InstansiateMediaLibraryObject(item, MediaLibraryHelper.GetBaseKey(Key), this));
+        }
+        catch (Exception e)
+        {
+          ServiceRegistration.Get<ILogger>().Error("Search failed adding media item {0}", e, item.MediaItemId);
+        }
+      }
+      return result;
+    }
+  }
+}
---
+++ MediaPortal/Incubator/MediaServer/Parser/UPnP.g4.lexer.cs
@@ -0,0 +1,6 @@
+﻿namespace MediaPortal.Plugins.MediaServer.ANTLR
+{
+    partial class UPnPLexer
+    {
+    }
+}
